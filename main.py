673068c7f9c41b3608ac8d20d2bcba2304c63ed6
Muhammad Asim <47195175+muhamdasim@users.noreply.github.com>
2020-11-11 03:15:32 +0500

    Initial Commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import RetriveData as DataFetcher
-
-
+import mondayConnection as Database
+import json
 #DataFetcher.storeData()
 DataFetcher.loadJson()
 #name
@@ -8,4 +8,20 @@ DataFetcher.loadJson()
 #location
 #DataFetcher.getLocation()
 #time
-DataFetcher.getTime()
+#DataFetcher.getTime()
+
+print("Connecting to Monday Database")
+try:
+    obj=Database.connection()
+
+except:
+    print("Unable to Connect to Monday :((")
+
+
+groups=obj.get_board(id=846185373).get_group(title='Pending')
+
+item_name='Asim'
+thisdict = {
+  "text60": "Ford"
+}
+groups.add_item("test",text60="finally", location_or_launchpad='fuck')
