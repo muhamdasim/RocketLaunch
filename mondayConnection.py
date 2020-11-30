add881dd688496dbb62c296f9bf0865c94f329c8
Muhammad Asim <47195175+muhamdasim@users.noreply.github.com>
2020-11-30 12:10:54 +0500

    Update mondayConnection.py

--- mondayConnection.py
+++ mondayConnection.py
@@ -2,7 +2,7 @@ from moncli import MondayClient
 
 
 def connection():
-    return MondayClient(user_name='muhamdasim.business@gmail.com', api_key_v1='', api_key_v2='eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjg4OTA3ODQ3LCJ1aWQiOjE2Nzc4ODc2LCJpYWQiOiIyMDIwLTEwLTIzVDA5OjU1OjQ3LjAwMFoiLCJwZXIiOiJtZTp3cml0ZSJ9.7vjO6PiYT_GBlvBTZfjrOFGSnMNq7pNwiuBrh8NgqNk')
+    return MondayClient(user_name='Your Username', api_key_v1='', api_key_v2='Your Api Key;')
 
 
 
